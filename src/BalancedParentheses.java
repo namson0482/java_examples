9f06cb301f5cc71bad7fba10d254a0521641cd29
Vu Nam Son <namson0482@gmail.com>
2017-07-08 17:10:05 +0700

    Initial Projects

---
+++ src/BalancedParentheses.java
@@ -0,0 +1,109 @@
+import java.util.Arrays;
+import java.util.List;
+
+
+public class BalancedParentheses {
+//
+	private static String[] checkBalancedParentheses(String[] values){
+		
+		String []result = new String[values.length];
+		for(int k=0;k<values.length;k++) {
+			String input = values[k];
+			String response = "NO";
+			if(input == null){
+	        	response = "NO";
+	        } else if(input.length()==0){
+	            response = "YES";
+	        } else {
+	        	java.util.Stack<String> stack = new java.util.Stack<String>();
+				for(int i=0; i < input.length(); i++){
+					String str = ""+input.charAt(i);		
+					if(str.equals("(") || str.equals("[") || str.equals("{")){
+						stack.push(str);
+					}
+					if(str.equals(")") || str.equals("]") || str.equals("}")){
+						if(stack.isEmpty()){
+							response = "NO";
+							break;
+						}
+						String opening = stack.peek();
+						if(str.equals(")") && opening.equals("(")){
+							stack.pop();
+						}
+						if(str.equals("]") && opening.equals("[")){
+							stack.pop();
+						}
+						if(str.equals("}") && opening.equals("{")){
+							stack.pop();
+						}
+					}			
+				}
+				if(input.length() > 0 && stack.isEmpty()){
+					response = "YES";
+				}
+	        }
+			result[k] = response;
+		}
+		return result;
+	}
+	
+	
+//	private static String[] checkBalancedParentheses(String[] values){
+//		String []result = new String[values.length];
+//		
+//		for(int k=0;k<values.length;k++) {
+//			String value = values[k];
+//			java.util.Stack<Character> specialCharStack = new java.util.Stack<Character>();
+//	        String response = "NO";
+//	        char tempChar;
+//	        Character[] openingBraces = {'[','(','{'};
+//	        Character[] closingBraces = {']',')','}'};
+//	        List<Character> openingBracesList = Arrays.asList(openingBraces);
+//	        List<Character> closingBracesList = Arrays.asList(closingBraces);
+//	        if(value == null){
+//	        	response = "NO";
+//	        } else if(value.length()==0){
+//	            response = "YES";
+//	        } else{
+//	            for(int i=0; i < value.length(); i++) {
+//	                tempChar = value.charAt(i);
+//	                 
+//	                if(openingBracesList.contains(tempChar)){
+//	                    specialCharStack.push(tempChar);
+//	                }else if(closingBracesList.contains(tempChar)){
+//	                    if(!specialCharStack.isEmpty()){
+//	                        if(tempChar==')' && '(' != specialCharStack.pop()){
+//	                            break;
+//	                        }else if(tempChar=='}' && '{' !=specialCharStack.pop()){
+//	                        	break;
+//	                        }else if(tempChar==']' && '[' != specialCharStack.pop()){
+//	                        	break;
+//	                        }
+//	                    }else{
+//	                    	break;
+//	                    }
+//	                }else{
+//	                	break;
+//	                }
+//	            }
+//	            if( specialCharStack.isEmpty()){
+//		            response = "YES";
+//		            
+//		        }
+//	        }
+//	        result[k] = response;
+//	        
+//		}
+//		return result;
+//		
+//	}
+
+	public static void main(String[] args) {
+		String[] inputs = null;//{"{[()]}","{[(])}","{{[[(())]]}}"};		
+		String []results = checkBalancedParentheses(inputs);
+		for(String input : results){
+			System.out.println(input);
+					
+		}
+	}
+}
---
+++ src/StockMaximize.java
@@ -0,0 +1,116 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class StockMaximize {
+	
+	public int value_1 = 0;
+	
+	public int value_2 = 0;
+	
+	public void func_1(int b) {
+		b = b * b;
+		System.out.println(b);
+	}
+	
+	public static final void func_2(StockMaximize s) {
+		s.value_1 = 10;
+	}
+	
+	private static long[] getElements(String elements, int size) {
+
+		String[] elementsArray = elements.trim().split(" ");
+
+		if (elementsArray.length != size)
+			throw new IllegalArgumentException("Number of elements in the list does not match the size provided");
+
+		long[] numbers = new long[size];
+		int index = 0;
+
+		// extract integers
+		for (String ele : elementsArray) {
+			try {
+				numbers[index++] = Long.parseLong(ele);
+			} catch (NumberFormatException nfe) {
+				System.out.println("Caught NumberFormatException: Unable to parse to long");
+				nfe.getStackTrace();
+			}
+
+		}
+		return numbers;
+	}
+
+	private static boolean[] findSellingDays(long[] stockPrices, int days) {
+
+		boolean[] sellOn = new boolean[days]; 
+		Arrays.fill(sellOn, false); 
+		long localMax = Long.MIN_VALUE; 
+		for (int i = days - 1; i >= 0; i--) {
+			if (localMax < stockPrices[i]) { 
+				localMax = stockPrices[i]; 
+				sellOn[i] = true; 
+			}
+		}
+		return sellOn;
+	}
+
+	
+	private static long calcMaxProfit(long[] stockPrices, int days) {
+
+		boolean[] sellOn = findSellingDays(stockPrices, days);
+
+		long totalCost = 0, totalProfit = 0;
+		int numShares = 0;
+
+		for (int i = 0; i < days; i++) {
+			if (sellOn[i] == false) { 
+				totalCost += stockPrices[i];
+				numShares++;
+			} else if (totalCost != 0) { 
+				totalProfit += numShares * stockPrices[i] - totalCost; 
+				totalCost = 0;
+				numShares = 0;
+			}
+		}
+		return totalProfit;
+	}
+
+	public static void main(String[] args) {
+		
+		StockMaximize s = new StockMaximize();
+		int a = 6;
+		s.func_1(a);
+		System.out.println(a);
+		System.out.println(s.value_1);
+		StockMaximize.func_2(s);
+		System.out.println(s.value_1);
+		/*BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			int testCases = Integer.parseInt(input.readLine().trim());
+
+			if (testCases <= 0) {
+				throw new IllegalArgumentException("The number of test cases must be greater than 0");
+			}
+
+			while (testCases > 0) {
+				int days = Integer.parseInt(input.readLine().trim());
+				if (days <= 0) {
+					throw new IllegalArgumentException("The number of test cases must be greater than 0");
+				}
+				long[] stockPrices = getElements(input.readLine(), days);
+
+				long maxProfit = calcMaxProfit(stockPrices, days);
+				System.out.println(maxProfit);
+				testCases--;
+			}
+			input.close();
+		} catch (NumberFormatException nfe) {
+			System.out.println("Caught NumberFormatException: Unable to parse inputs from stdin");
+			nfe.getStackTrace();
+		} catch (IOException ioe) {
+			System.out.println("Caught IOException: Unable to read inputs from stdin");
+			ioe.getStackTrace();
+		}*/
+	}
+}	
